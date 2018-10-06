219cb1cf8c8211c7be934d19771bc592cf942156
Stefan Ronacher <stefan.ronacher.78@gmail.com>
2018-10-06 16:33:32 -0300

    Probando github con Atom

---
+++ sql/script_entradas.php
@@ -0,0 +1,12 @@
+<?php
+$query = "INSERT INTO Entradas (Entradas_id,Eventos_id,Estado) VALUES";
+
+for($ent=1;$ent<=200;$ent++){
+  $query .= "($ent,1,'Libre'), \r\n";
+}
+
+for(;$ent<=350;$ent++){
+  $query .= "($ent,2,'Libre'), \r\n";
+}
+echo $query;
+?>
